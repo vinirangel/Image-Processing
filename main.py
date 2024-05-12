2491d68ef44c56b056f0f5416ca32415e519a20c
vinirangel <vinirangel@yahoo.com>
2024-05-12 00:01:42 -0300

    HSV Characteristics added to XGBoost

--- main.py
+++ main.py
@@ -30,7 +30,7 @@ from sklearn import preprocessing
 # test_size = 0.33
 # X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size=test_size, random_state=seed)
 
-main_layout = [[psg.Button("Carregar Imagem", key="-LOAD-")], 
+main_layout = [[psg.Button("Carregar Imagem", key="-LOAD-"), psg.VerticalSeparator(pad=10, color="gray"), psg.Button("Classificador XGBoost", key="-XGBOOST-")], 
          [psg.Button("Visualizar Imagem", key="-VIEW-")],
          [psg.Button("Tons de Cinza", key="-GRAYSCALE-")],
          [psg.Button("Histograma Tons de Cinza", key="-GRAYSCALE_HISTOGRAM-")],
@@ -47,106 +47,107 @@ file = "EMPTY"
 # file_path = "/home/vinicius/Desktop/PI/sample.jpg"
 file_path = "C:/Users/Vinicius/Desktop/CS Files/PI/sample.jpg"
 
-train_images = []
-train_labels = []
+# train_images = []
+# train_labels = []
 
-for directory_path in glob.glob("C:/Users/Vinicius/Desktop/CS Files/PI/Image-Processing/Training/*"):
-   label = directory_path.split("\\")[-1]
-   # print(label)
-   for img_path in glob.glob(os.path.join(directory_path, "*png")):
-      print(img_path)
-      img = cv2.imread(img_path)
-      img = cv2.resize(img, (100,100))
-      # img = cv2.cvtColor(img, cv2.COLOR_RGB2BGR)
-      train_images.append(img)
-      train_labels.append(label)
+# for directory_path in glob.glob("C:/Users/Vinicius/Desktop/CS Files/PI/Image-Processing/Training/*"):
+#    label = directory_path.split("\\")[-1]
+#    # print(label)
+#    for img_path in glob.glob(os.path.join(directory_path, "*png")):
+#       print(img_path)
+#       img = cv2.imread(img_path)
+#       img = cv2.resize(img, (100,100))
+#       # img = cv2.cvtColor(img, cv2.COLOR_RGB2BGR)
+#       train_images.append(img)
+#       train_labels.append(label)
 
-train_images = np.array(train_images)
-train_labels = np.array(train_labels)
+# train_images = np.array(train_images)
+# train_labels = np.array(train_labels)
 
-test_images = []
-test_labels = []
+# test_images = []
+# test_labels = []
 
-for directory_path in glob.glob("C:/Users/Vinicius/Desktop/CS Files/PI/Image-Processing/Validation/*"):
-   label = directory_path.split("\\")[-1]
-   # print(label)
-   for img_path in glob.glob(os.path.join(directory_path, "*png")):
-      print(img_path)
-      img = cv2.imread(img_path, cv2.IMREAD_COLOR)
-      img = cv2.resize(img, (100,100))
-      # img = cv2.cvtColor(img, cv2.COLOR_RGB2BGR)
-      test_images.append(img)
-      test_labels.append(label)
+# for directory_path in glob.glob("C:/Users/Vinicius/Desktop/CS Files/PI/Image-Processing/Validation/*"):
+#    label = directory_path.split("\\")[-1]
+#    # print(label)
+#    for img_path in glob.glob(os.path.join(directory_path, "*png")):
+#       print(img_path)
+#       img = cv2.imread(img_path, cv2.IMREAD_COLOR)
+#       img = cv2.resize(img, (100,100))
+#       # img = cv2.cvtColor(img, cv2.COLOR_RGB2BGR)
+#       test_images.append(img)
+#       test_labels.append(label)
 
-test_images = np.array(test_images)
-test_labels = np.array(test_labels)
+# test_images = np.array(test_images)
+# test_labels = np.array(test_labels)
 
-#Encode labels from test to integers
-le = preprocessing.LabelEncoder()
-le.fit(test_labels)
-test_labels_encoded = le.transform(test_labels)
-le.fit(train_labels)
-train_labels_encoded = le.transform(train_labels)
+# #Encode labels from test to integers
+# le = preprocessing.LabelEncoder()
+# le.fit(test_labels)
+# test_labels_encoded = le.transform(test_labels)
+# le.fit(train_labels)
+# train_labels_encoded = le.transform(train_labels)
 
-base_model = tf.keras.applications.VGG16(include_top=False,
-                                               weights='imagenet')
-base_model.trainable = False
-base_model.summary()
+# base_model = tf.keras.applications.VGG16(include_top=False,
+#                                                weights='imagenet')
+# base_model.trainable = False
+# base_model.summary()
 
-train_features = base_model.predict(train_images)
-train_features = train_features.reshape(train_features.shape[0], -1)
-val_features = base_model.predict(test_images)
-val_features = val_features.reshape(val_features.shape[0], -1)
+# train_features = base_model.predict(train_images)
+# train_features = train_features.reshape(train_features.shape[0], -1)
+# val_features = base_model.predict(test_images)
+# val_features = val_features.reshape(val_features.shape[0], -1)
 
-model = xgb.XGBClassifier(learning_rate=0.1, n_estimators=120)
+# model = xgb.XGBClassifier(learning_rate=0.1, n_estimators=120)
 
-eval_set = [(train_features, train_labels_encoded), (val_features, test_labels_encoded)]
+# # eval_set = [(train_features, train_labels_encoded), (val_features, test_labels_encoded)]
+# eval_set = [(train_features, train_labels_encoded), (val_features, test_labels_encoded)]
 
-model.fit(
-    train_features, 
-    train_labels_encoded,  
-    eval_metric=["merror"], 
-    eval_set=eval_set, 
-    verbose=True
-)
+# model.fit(
+#     train_features, 
+#     train_labels_encoded,  
+#     eval_metric=["merror"], 
+#     eval_set=eval_set, 
+#     verbose=True
+# )
 
-results = model.evals_result()
+# results = model.evals_result()
 
-train_error = results['validation_0']['merror']
-train_acc = [1.0 - i for i in train_error]
+# train_error = results['validation_0']['merror']
+# train_acc = [1.0 - i for i in train_error]
 
-val_error = results['validation_1']['merror']
-val_acc = [1.0 - i for i in val_error]
+# val_error = results['validation_1']['merror']
+# val_acc = [1.0 - i for i in val_error]
 
-# best_ntree_limit = model.best_ntree_limit()
-# print('Best ntree limit: ', best_ntree_limit)
+# # best_ntree_limit = model.best_ntree_limit()
+# # print('Best ntree limit: ', best_ntree_limit)
 
-plt.figure(figsize=(8, 8))
-plt.subplot(2, 1, 1)
-plt.plot(train_acc, label='Train Accuracy')
-plt.plot(val_acc, label='Validation Accuracy')
-plt.legend(loc='lower right')
-plt.ylabel('Accuracy')
-plt.ylim([0.3,1.1])
-# plt.axvline(best_ntree_limit-1, color="gray", label="Optimal tree number")
-plt.title('Training and Validation Accuracy')
+# plt.figure(figsize=(8, 8))
+# plt.subplot(2, 1, 1)
+# plt.plot(train_acc, label='Train Accuracy')
+# plt.plot(val_acc, label='Validation Accuracy')
+# plt.legend(loc='lower right')
+# plt.ylabel('Accuracy')
+# plt.ylim([0.3,1.1])
+# # plt.axvline(best_ntree_limit-1, color="gray", label="Optimal tree number")
+# plt.title('Training and Validation Accuracy')
 
-plt.subplot(2, 1, 2)
-plt.plot(train_error, label='Training Loss')
-plt.plot(val_error, label='Validation Loss')
-plt.legend()
-plt.title('Training and Validation Loss')
-# plt.axvline(best_ntree_limit-1, color="gray", label="Optimal tree number")
-plt.ylabel('merror')
-plt.ylabel('Number of Trees')
+# plt.subplot(2, 1, 2)
+# plt.plot(train_error, label='Training Loss')
+# plt.plot(val_error, label='Validation Loss')
+# plt.legend()
+# plt.title('Training and Validation Loss')
+# # plt.axvline(best_ntree_limit-1, color="gray", label="Optimal tree number")
+# plt.ylabel('merror')
+# plt.ylabel('Number of Trees')
 
-plt.show()
+# plt.show()
 
-train_predictions = model.predict(train_features)
-val_predictions = model.predict(val_features)
+# train_predictions = model.predict(train_features)
+# val_predictions = model.predict(val_features)
 
-print ("Training Accuracy = ", accuracy_score(train_labels_encoded, train_predictions))
-print ("Validation Accuracy = ", accuracy_score(test_labels_encoded, val_predictions))
+# print ("Training Accuracy = ", accuracy_score(train_labels_encoded, train_predictions))
+# print ("Validation Accuracy = ", accuracy_score(test_labels_encoded, val_predictions))
 
 while True:
    event, values = window.read()
@@ -222,7 +223,7 @@ while True:
       img = cv2.imread(file_path)
       assert img is not None, "file could not be read"
       hsv = cv2.cvtColor(img,cv2.COLOR_BGR2HSV)
-      hist = cv2.calcHist( [hsv], [0, 1], None, [180, 256], [0, 16, 0, 256] )
+      hist = cv2.calcHist( [hsv], [0, 2], None, [16, 8], [0, 180, 0, 256] )
       plt.imshow(hist,interpolation = 'nearest')
       plt.show()
 
@@ -237,13 +238,13 @@ while True:
       #Calcula as matrizes de Co Ocorrencia
       #C1,1
       glcm1 = skimage.feature.graycomatrix(img_array, distances=[1,1], angles=[3*np.pi/2], symmetric=False, normed=False)
-      # print("C1,1 \n", glcm1[0:5, 0:5])
+      print("C1,1 \n", glcm1[0:5, 0:5])
       #C2,2
       glcm2 = skimage.feature.graycomatrix(img_array, distances=[2,2], angles=[3*np.pi/2], symmetric=False, normed=False)      
       # print("C2,2 \n", glcm2[0:5, 0:5])
       #C4,4
       glcm4 = skimage.feature.graycomatrix(img_array, distances=[4,4], angles=[3*np.pi/2], symmetric=False, normed=False)
-      print("C4,4 \n", glcm4[0:5, 0:5])
+      # print("C4,4 \n", glcm4[0:5, 0:5])
       #C8,8
       glcm8 = skimage.feature.graycomatrix(img_array, distances=[8,8], angles=[3*np.pi/2], symmetric=False, normed=False)
       #C16,16
@@ -253,7 +254,7 @@ while True:
 
       #Calcular a Entropia 
       entropy1 = skimage.measure.shannon_entropy(glcm1)
-      # print("Entropy 1,1 ",entropy1)
+      print("Entropy 1,1 ",entropy1)
       entropy2 = skimage.measure.shannon_entropy(glcm2)
       # print("Entropy 2,2 ",entropy2)
       entropy4 = skimage.measure.shannon_entropy(glcm4)
@@ -263,7 +264,7 @@ while True:
 
       #Calcular a Homogeneidade
       homogeneity1 = skimage.feature.graycoprops(glcm1, 'homogeneity')
-      # print("HOMOGENEITY1,1 ", homogeneity1)
+      print("HOMOGENEITY1,1 ", homogeneity1)
       homogeneity2 = skimage.feature.graycoprops(glcm2, 'homogeneity')
       homogeneity4 = skimage.feature.graycoprops(glcm2, 'homogeneity')
       homogeneity8 = skimage.feature.graycoprops(glcm8, 'homogeneity')
@@ -272,7 +273,7 @@ while True:
 
       #Calcular o Contraste
       contrast1 = skimage.feature.graycoprops(glcm1, 'contrast')
-      # print("CONTRAST1,1 ", contrast1)
+      print("CONTRAST1,1 ", contrast1)
       contrast2 = skimage.feature.graycoprops(glcm2, 'contrast')
       contrast4 = skimage.feature.graycoprops(glcm4, 'contrast')
       contrast8 = skimage.feature.graycoprops(glcm8, 'contrast')
@@ -298,6 +299,135 @@ while True:
       cv2.imshow("Hu-Moments", img)
       cv2.waitKey(0)
       cv2.destroyAllWindows()
+
+   if event == "-XGBOOST-":
+      img = cv2.imread(file_path)
+      assert img is not None, "file could not be read"
+      hsv = cv2.cvtColor(img,cv2.COLOR_BGR2HSV)
+      hbins = 16 
+      vbins = 8
+      histSize = {hbins, vbins}
+      hist = cv2.calcHist( [hsv], [0, 2], None, [16, 8], [0, 180, 0, 256] )
+      # print(hist)
+
+      hbins = 16 
+      vbins = 8
+      histSize = {hbins, vbins}
+
+      train_images = []
+      train_labels = []
+
+      for directory_path in glob.glob("C:/Users/Vinicius/Desktop/CS Files/PI/Image-Processing/Training/*"):
+         label = directory_path.split("\\")[-1]
+         # print(label)
+         for img_path in glob.glob(os.path.join(directory_path, "*png")):
+            print(img_path)
+            img = cv2.imread(img_path)
+            img = cv2.resize(img, (100,100))
+            hsv = cv2.cvtColor(img,cv2.COLOR_BGR2HSV)
+            hist = cv2.calcHist( [hsv], [0, 2], None, [16, 8], [0, 180, 0, 256] )
+            # img = cv2.cvtColor(img, cv2.COLOR_RGB2BGR)
+            train_images.append(hist)
+            train_labels.append(label)
+
+      train_images = np.array(train_images)
+      train_labels = np.array(train_labels)
+
+      test_images = []
+      test_labels = []
+
+      for directory_path in glob.glob("C:/Users/Vinicius/Desktop/CS Files/PI/Image-Processing/Validation/*"):
+         label = directory_path.split("\\")[-1]
+         # print(label)
+         for img_path in glob.glob(os.path.join(directory_path, "*png")):
+            print(img_path)
+            img = cv2.imread(img_path, cv2.IMREAD_COLOR)
+            img = cv2.resize(img, (100,100))
+            hsv = cv2.cvtColor(img,cv2.COLOR_BGR2HSV)
+            hist = cv2.calcHist( [hsv], [0, 2], None, [16, 8], [0, 180, 0, 256] )
+            # img = cv2.cvtColor(img, cv2.COLOR_RGB2BGR)
+            test_images.append(hist)
+            test_labels.append(label)
+
+      test_images = np.array(test_images)
+      test_labels = np.array(test_labels)
+
+      #Encode labels from test to integers
+      le = preprocessing.LabelEncoder()
+      le.fit(test_labels)
+      test_labels_encoded = le.transform(test_labels)
+      le.fit(train_labels)
+      train_labels_encoded = le.transform(train_labels)
+
+      base_model = tf.keras.applications.VGG16(include_top=False,
+                                                     weights='imagenet')
+      base_model.trainable = False
+      base_model.summary()
+
+      # train_features = base_model.predict(train_images)
+      # train_features = train_features.reshape(train_features.shape[0], -1)
+      # val_features = base_model.predict(test_images)
+      # val_features = val_features.reshape(val_features.shape[0], -1)
+
+      train_images = train_images.reshape(train_images.shape[0], -1)
+      test_images = test_images.reshape(test_images.shape[0], -1)
+
+      model = xgb.XGBClassifier(learning_rate=0.1, n_estimators=120)
+
+      # eval_set = [(train_features, train_labels_encoded), (val_features, test_labels_encoded)]
+      eval_set = [(train_images, train_labels_encoded), (test_images, test_labels_encoded)]
+
+      model.fit(
+          train_images, 
+          train_labels_encoded,  
+          eval_metric=["merror"], 
+          eval_set=eval_set, 
+          verbose=True
+      )
+
+      results = model.evals_result()
+
+      train_error = results['validation_0']['merror']
+      train_acc = [1.0 - i for i in train_error]
+
+      val_error = results['validation_1']['merror']
+      val_acc = [1.0 - i for i in val_error]
+
+      # best_ntree_limit = model.best_ntree_limit()
+      # print('Best ntree limit: ', best_ntree_limit)
+
+      plt.figure(figsize=(8, 8))
+      plt.subplot(2, 1, 1)
+      plt.plot(train_acc, label='Train Accuracy')
+      plt.plot(val_acc, label='Validation Accuracy')
+      plt.legend(loc='lower right')
+      plt.ylabel('Accuracy')
+      plt.ylim([0.3,1.1])
+      # plt.axvline(best_ntree_limit-1, color="gray", label="Optimal tree number")
+      plt.title('Training and Validation Accuracy')
+
+      plt.subplot(2, 1, 2)
+      plt.plot(train_error, label='Training Loss')
+      plt.plot(val_error, label='Validation Loss')
+      plt.legend()
+      plt.title('Training and Validation Loss')
+      # plt.axvline(best_ntree_limit-1, color="gray", label="Optimal tree number")
+      plt.ylabel('merror')
+      plt.ylabel('Number of Trees')
+
+      plt.show()
+
+      # train_predictions = model.predict(train_features)
+      # val_predictions = model.predict(val_features)
+
+      # print ("Training Accuracy = ", accuracy_score(train_labels_encoded, train_predictions))
+      # print ("Validation Accuracy = ", accuracy_score(test_labels_encoded, val_predictions))
+
+      train_predictions = model.predict(train_images)
+      val_predictions = model.predict(test_images)
+
+      print ("Training Accuracy = ", accuracy_score(train_labels_encoded, train_predictions))
+      print ("Validation Accuracy = ", accuracy_score(test_labels_encoded, val_predictions))
 
    if event == psg.WIN_CLOSED:
       break
